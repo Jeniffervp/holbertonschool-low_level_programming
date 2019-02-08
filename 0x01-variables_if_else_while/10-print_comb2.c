b1e3ed22b9c87bcddd1951bd6b1116c6916ab720
jeniffervp <737@holbertonschool.com>
2019-02-08 20:53:24 +0000

    today work

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -11,17 +11,19 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 100; num++)
+	while (num < 100)
 	{
 		putchar((num / 10) + '0');
 		putchar((num % 10) + '0');
-		if (num < 99)
+		if (num != 99)
 		{
 			putchar(',');
 			putchar(' ');
 		}
+		num++;
 	}
 	putchar('\n');
 
 	return (0);
 }
+
