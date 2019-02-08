4f39204b92f3f458bf2f942a3ade98a23a324926
jeniffervp <737@holbertonschool.com>
2019-02-08 01:55:06 +0000

    if

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+		putchar(num);
+	for (num = 'a'; num <= 'f'; num++)
+		putchar(num);
+	putchar('\n');
+
+	return (0);
+}
+
