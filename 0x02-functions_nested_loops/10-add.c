f932d7470e0b1116009dcc86f7d1d264718fe0fa
jeniffervp <737@holbertonschool.com>
2019-02-13 00:56:48 +0000

    twoint

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - entry point
+ *@a: this is  a number
+ *@b: this is a number
+ * Description: adds two integers
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
