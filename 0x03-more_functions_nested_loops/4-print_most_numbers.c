49e4f19078f04d5c81e55835bb989fd9b2c17681
jeniffervp <737@holbertonschool.com>
2019-02-14 19:41:13 +0000

    print numbers but no 2 and 4

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - entry point
+ *
+ * Description: print Holberton
+ * Return: alwais 0
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (!((a == 50) || (a == 52)))
+		{
+			_putchar(a);
+		}
+	}
+
+	_putchar(10);
+	return (0);
+}
