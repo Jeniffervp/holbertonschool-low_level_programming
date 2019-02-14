d10f5784bdf624f95da0647639d6ee80941ca171
jeniffervp <737@holbertonschool.com>
2019-02-14 15:57:06 +0000

    a x b

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - entry point
+ *@a: is a number
+ *@b: is a number
+ * Description: print Holberton
+ * Return: 1 o 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
