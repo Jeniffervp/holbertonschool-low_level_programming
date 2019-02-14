218f7323cc8547c1920f52609337a34065460a25
jeniffervp <737@holbertonschool.com>
2019-02-14 18:44:38 +0000

    numbers

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - entry point
+ *
+ * Description: print Holberton
+ * Return: alwais 0
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar(10);
+}
