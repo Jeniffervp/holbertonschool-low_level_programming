ce9f166e2bb1df00e5c188b23460790c3bd80ef7
jeniffervp <737@holbertonschool.com>
2019-02-14 15:25:08 +0000

    is upper

---
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _isupper - entry point
+ *@c: is a upper letter
+ * Description: print Holberton
+ * Return: 1 o 0
+ */
+
+int _isupper(int c)
+{
+	if ((c == 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+
+	return (0);
+}
