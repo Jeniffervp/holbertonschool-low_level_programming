5d4f1933f670134f63f3a232e149ae6ace824802
jeniffervp <737@holbertonschool.com>
2019-02-15 01:32:56 +0000

    fizz buzz

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print Holberton
+ * Return: alwais 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+
+		if ((a % 3 == 0) && (a % 5 == 0))
+			printf("FizzBuzz ");
+		else if (a % 3 == 0)
+			printf("Fizz ");
+		else if ((a % 5 == 0) && (a != 100))
+			printf("Buzz ");
+		else if (a == 100)
+			printf("Buzz");
+		else
+			printf(" %d ", a);
+	}
+
+	printf("\n");
+
+	return (0);
+}
