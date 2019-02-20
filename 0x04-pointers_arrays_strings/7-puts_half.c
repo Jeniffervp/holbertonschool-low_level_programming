897c93556cd558795d6a1728c47ba9ebbca9ec6f
jeniffervp <737@holbertonschool.com>
2019-02-20 02:03:07 +0000

    second half

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * puts_half - entry point
+ *@str: are numbers
+ * Description: the second half
+ * Return: always 0
+ */
+
+void puts_half(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != '\0'; c++)
+	{
+	}
+	if (c % 2 == 0)
+	{
+		c = c / 2;
+	}
+	else
+		c = (c - 1) / 2;
+	for ( ; str[c] != '\0'; c++)
+	{
+		_putchar(str[c]);
+	}
+
+	_putchar(10);
+}
