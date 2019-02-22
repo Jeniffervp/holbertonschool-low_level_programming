4bd5ad0460c512c8abe872b73e962105f68bf31d
jeniffervp <737@holbertonschool.com>
2019-02-22 00:14:23 +0000

    change to upper

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *string_toupper - entry point
+ * Return: always 0
+ */
+
+char *string_toupper(char *a)
+{
+	int  b = 0;
+
+	while (a[b])
+	{
+		if (a[b] >= 97 && a[b] <= 122)
+		{
+			a[b] = a[b] - 32;
+		}
+
+		b++;
+	}
+	return (a);
+}
