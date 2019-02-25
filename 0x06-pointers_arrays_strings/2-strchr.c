6adc32c11983ce0b3b242c710fa05cfeb58dacce
jeniffervp <737@holbertonschool.com>
2019-02-25 22:07:58 +0000

    position of c

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+  *_strchr - entri point
+  *@s: is the pointer
+  *@c: is the string
+  * Return: *s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	for (a = 0; s[a]; a++)
+	{
+		if (s[a] == c)
+			return (&s[a]);
+
+	}
+	if (s[a] == '\0')
+	{
+		return (&s[a]);
+	}
+	return (0);
+}
