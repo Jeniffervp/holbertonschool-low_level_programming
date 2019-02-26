34d7341192332cf650994d4679277ed5c25d34c1
jeniffervp <737@holbertonschool.com>
2019-02-26 13:31:31 +0000

    set the value

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+  *set_string - entri point
+  *@s: is the pointer that recive the value.
+  *@to: is the pointer that give the value.
+  * Return: count
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
