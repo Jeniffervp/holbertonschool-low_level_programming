2c8c977ee5094f5f9d6b10784725adc678d2c157
jeniffervp <737@holbertonschool.com>
2019-02-26 00:55:32 +0000

    print since the first coincidence

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+  *_strpbrk - entri point
+  *@s: is the pointer
+  *@accept: is another pointer
+  * Return: count
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int first, second;
+
+	for (first = 0; s[first]; first++)
+	{
+		for (second = 0; accept[second]; second++)
+		{
+			if (accept[second] == s[first])
+				return (s + first);
+		}
+	}
+	if (accept[second] == s[first])
+		return (s + first);
+	return (0);
+}
