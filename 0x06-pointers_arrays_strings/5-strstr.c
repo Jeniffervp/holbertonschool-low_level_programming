fb0037e70e798f851b91a92d282e7f38e52c86de
jeniffervp <737@holbertonschool.com>
2019-02-26 03:31:41 +0000

    fist coincidence in substring

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+ #include "holberton.h"
+#include <stdio.h>
+/**
+  *_strstr - entri point
+  *@haystack: is the pointer
+  *@needle: is another pointer
+  * Return: count
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int f = 0, s = 0;
+
+	if (*needle == 0)
+		return (haystack);
+
+	for (; haystack[0] != '\0' ; haystack++)
+	{
+		while (haystack[f] == needle[s])
+		{
+			s++;
+			f++;
+			if (needle[s] == '\0')
+				return (haystack);
+		}
+		f = 0;
+		s = 0;
+	}
+	return (0);
+}
