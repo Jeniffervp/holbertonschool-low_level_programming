ea99c28ce50b03b8db67db08fd7a27afaeadbfb0
jeniffervp <737@holbertonschool.com>
2019-02-28 22:06:18 +0000

    square root

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,28 +1,34 @@
 #include "holberton.h"
 
 /**
-  * _sqrt_recursion - entri point
-  * aux - other entri point
+  *aux - entri point
   *@n: is a number
   *@num: the other number
   * Description: returns the natural square root of a number.
   * Return: something
  */
 
-int aux (int num, int n)
+int aux(int num, int n)
 {
 	if (n < 0)
 		return (-1);
 	if (num * num == n)
 		return (num);
 	if (num * num < n)
-		return (aux (num + 1, n));
+		return (aux(num + 1, n));
 	else
 		return (-1);
 	return (0);
 }
 
+/**
+ * _sqrt_recursion - entri point
+ *@n: is a number
+ * Description: returns the natural square root of a number.
+ * Return: something
+*/
+
 int _sqrt_recursion(int n)
 {
-	return (aux (1, n));
+	return (aux(1, n));
 }
