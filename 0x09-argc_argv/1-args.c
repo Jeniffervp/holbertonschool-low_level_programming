54e2e94250b153b3eb05c9fcfa99606fd11869ac
jeniffervp <737@holbertonschool.com>
2019-03-03 00:36:20 +0000

    number of argc

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *@argc: Argument
+  *@argv: Pointer to the string
+  * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+
+}
