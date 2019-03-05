c24f3b0b8828b0e68ee157d32255a6c10a80807d
jeniffervp <737@holbertonschool.com>
2019-03-05 21:08:20 +0000

    duplicate

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - duplicate a string to new memory space.
+ * @str: the address of memory to duplicate.
+ *
+ * Return: Nothing.
+ */
+
+char *_strdup(char *str)
+{
+	char *x;
+	int y;
+
+	if (*str == 0)
+		return (0);
+
+	x = malloc(sizeof(char));
+
+	if (str > x)
+		return (0);
+
+	for (y = 0; str[y]; y++)
+		x[y] = str[y];
+	return (x);
+}
