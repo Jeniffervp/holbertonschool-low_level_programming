4713850d870c14fce2b2116421ac5fcb00109880
jeniffervp <737@holbertonschool.com>
2019-03-07 23:51:29 +0000

    calloc

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ *_calloc -concatenate two strings.
+ * @nmemb: elements to be allocated..
+ * @size: size of elements.
+ *
+ * Return: Nothing.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *a, b;
+
+	if ((nmemb == '\0') || if (size == '\0'))
+		return (0);
+
+	a = malloc(sizeof(size * nmemb);
+	if (a == '\0')
+		return (0);
+	for (b = 0; b < (size * nmemb); b++)
+		a[b] = '\0';
+	return (a);
+}
