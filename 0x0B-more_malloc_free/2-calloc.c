ba7ccb023aceeffc0fc1d45e7af55c17d8cd1113
jeniffervp <737@holbertonschool.com>
2019-03-08 00:00:07 +0000

    calloc

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -14,10 +14,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *a;
 	unsigned char b;
 
-	if ((nmemb == '\0') || if (size == '\0'))
+	if ((nmemb == '\0') || (size == '\0'))
 		return (0);
 
-	a = malloc(sizeof(size * nmemb);
+	a = malloc(size * nmemb);
 	if (a == '\0')
 		return (0);
 	for (b = 0; b < (size * nmemb); b++)
