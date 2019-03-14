39b86b85ed938504344c28757e5487e887db63d9
jeniffervp <737@holbertonschool.com>
2019-03-14 01:23:35 +0000

    simple operations

---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+
+/**
+ *main - entri point.
+ * @argc: is a counter.
+ * @argv: is a argument.
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int j;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((argv[2][0] != '+') && (argv[2][0] != '-') && (argv[2][0] != '*') &&
+	    (argv[2][0] != '/') && (argv[2][0] != '%'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (((argv[2][0] == '/') || (argv[2][0] == '%')) && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	j = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", j);
+
+	return (0);
+}
---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ *op_add - Option to add
+ * @a: a number.
+ * @b: a number.
+ *
+ * Return: a + b.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - Option to sub
+ * @a: a number.
+ * @b: a number.
+ *
+ * Return: a - b.
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - Option to mul
+ * @a: a number.
+ * @b: a number.
+ *
+ * Return: a * b.
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - Option to div
+ * @a: a number.
+ * @b: a number.
+ *
+ * Return: a * b.
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - Option to mod
+ * @a: a number.
+ * @b: a number.
+ *
+ * Return: a * b.
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
