576e263f2f208282e3249e6fc1a9bc54c0c00825
jeniffervp <737@holbertonschool.com>
2019-03-15 23:04:02 +0000

    using variadic to made a function to print a string

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ *print_numbers: function to sum all the parameters.
+ *@separator: is the ",".
+ *@n: contain the numbers
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list a;
+	unsigned int b;
+	char *c;
+
+	va_start(a, n);
+
+	for (b = 0; b < n; b++)
+	{
+		c = va_arg(a, char *);
+		if (c == NULL)
+			printf("(nil)");
+		else
+			printf("%s", c);
+		if (separator != NULL)
+		{
+			if (b < n - 1)
+				printf("%s", separator);
+		}
+	}
+	va_end(a);
+	printf("\n");
+}
