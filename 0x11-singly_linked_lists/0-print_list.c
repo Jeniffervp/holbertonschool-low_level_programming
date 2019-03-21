ecc5a602c9f6b0f425a2b5d42b6cfbee080dcfe4
jeniffervp <737@holbertonschool.com>
2019-03-21 20:49:23 +0000

    [feature]print all the elements of a lists

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - print all the elements of a list..
+ *@h: coontain the elements to print.
+ *
+ * Return: The numbers of nodes.
+ */
+
+size_t print_list(const list_t *h)
+{
+
+	int count = 0;
+
+	while (h != NULL)
+	{
+		if (h == NULL)
+			printf("[%u] %p\n", 0, h->str);
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
