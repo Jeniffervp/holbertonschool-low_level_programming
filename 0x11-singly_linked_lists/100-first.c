33dec8ebd5ee122e7350ac17f84f244fbb58e9e9
jeniffervp <737@holbertonschool.com>
2019-03-22 01:22:18 +0000

    [feature]print before the main

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * myStart - print a string.
+ *
+ * Return: The numbers of nodes.
+ */
+
+
+void __attribute__((constructor)) myStart(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
