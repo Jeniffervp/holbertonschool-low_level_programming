d11ff12f9e4ac1fde71b9e6d904279e4ce0f2bbc
jeniffervp <737@holbertonschool.com>
2019-03-26 19:52:23 +0000

    [feature]sum the list

---
+++ 0x12-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_listint -  sum all data.
+ *@head: is the beginning of the list.
+ *
+ * Return: The numbers of nodes.
+ */
+
+int sum_listint(listint_t *head)
+{
+
+	int sum;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
