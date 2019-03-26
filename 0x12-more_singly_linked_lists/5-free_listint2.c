145ae5f7f07051bc4e0abb9937470c2e5793cefa
jeniffervp <737@holbertonschool.com>
2019-03-26 15:48:20 +0000

    [fix]head equal to NULL

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -14,13 +14,10 @@ void free_listint2(listint_t **head)
 	if (*head == NULL || head == NULL)
 		return;
 
-	while ((*head)->next != NULL)
+	while (*head != NULL)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
-	free((*head)->next);
-	free(*head);
-	*head = NULL;
 }
