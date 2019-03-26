fb08debb763b3d0194dd3acb84635cdbd8e9b2d7
jeniffervp <737@holbertonschool.com>
2019-03-26 16:12:17 +0000

    [fix]all

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -1,23 +1,22 @@
 #include "lists.h"
 
 /**
- * add_nodeint -  adds a new node at the beginning of a  list.
+ * pop_listint -  delete node at the beginning of a  list.
  *@head: is the beginning of the list.
- *@n: contain the integer.
  *
  * Return: The numbers of nodes.
  */
 
 int pop_listint(listint_t **head)
 {
-	int old_n = (*head)->n;
+	listint_t *temp;
+	int old_n;
 
-	if (*head == NULL)
-	{
-		free(*head);
+	if (*head == NULL || head == NULL)
 		return (0);
-	}
+	temp = (*head)->next;
+	old_n = (*head)->n;
 	free(*head);
-	*head = (*head)->next;
+	*head = temp;
 	return (old_n);
 }
