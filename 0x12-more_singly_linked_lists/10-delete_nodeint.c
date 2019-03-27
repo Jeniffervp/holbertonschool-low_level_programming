3deb8087f89a7cbd7976a5f5c663a7aa4947ab2c
jeniffervp <737@holbertonschool.com>
2019-03-27 01:48:05 +0000

    [Feature] delete a given node

---
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index -  delete a  node.
+ *@head: is the beginning of the list.
+ *@index: index of the node.
+ *
+ * Return: The numbers of nodes.
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, *current;
+	unsigned int aux;
+
+	if (*head == NULL)
+		return (-1);
+
+	temp = *head;
+	if (index == 0)
+	{
+		*head = temp->next;
+		free(temp);
+		return (1);
+	}
+	while (aux < index - 1)
+	{
+		if (temp == NULL || temp->next == NULL)
+			return (-1);
+		temp = temp->next;
+		aux++;
+	}
+	current = temp->next->next;
+	free(temp->next);
+	temp->next = current;
+	return (1);
+}
