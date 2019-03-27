14fff9b6f8d380b0a23e376c7969d39e3eccc4a6
jeniffervp <737@holbertonschool.com>
2019-03-27 01:51:08 +0000

    [Fix] aux = 0

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp, *current;
-	unsigned int aux;
+	unsigned int aux = 0;
 
 	if (*head == NULL)
 		return (-1);
