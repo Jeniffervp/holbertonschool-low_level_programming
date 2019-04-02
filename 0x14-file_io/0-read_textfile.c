9ce4651ece0810b70534090e5f76f2c31e158edf
jeniffervp <737@holbertonschool.com>
2019-04-02 23:34:33 +0000

    [FEATURE]read a text file

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and prints.
  *@filename: name of the file to create.
  *@letters: is the number of letters it should read and print.
  * Return: 1 success, -1 failure.
