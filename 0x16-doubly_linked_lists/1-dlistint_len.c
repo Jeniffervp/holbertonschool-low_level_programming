c2885a676fb0c7fd060c33c1c650ce439063d46e
jeniffervp <737@holbertonschool.com>
2019-05-04 17:15:21 +0000

    [fix] description of prototype

--- 0x16-doubly_linked_lists/1-dlistint_len.c
+++ 0x16-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_dlistint - returns the number of elements in a linked list.
+ * dlistint_len - returns the number of elements in a linked list.
  * @h: the list.
  * Return:  the number of elements.
  */
