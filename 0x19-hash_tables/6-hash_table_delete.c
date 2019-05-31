8882d638306d8b13bf6c759d440599c70b450ef4
jeniffervp <737@holbertonschool.com>
2019-05-31 20:38:05 +0000

    [feature]deletes a hash table.

---
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: hash table.
+ * Return: value associated with the element,or NULL if key not found
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int index;
+	hash_node_t *tempo, *aux;
+
+	if (ht == NULL)
+		return;
+	for (index = 0; index < ht->size; index++)
+	{
+		for (tempo = ht->array[index]; tempo; )
+		{
+			aux = tempo;
+			tempo = tempo->next;
+			free(aux->value);
+			free(aux->key);
+			free(aux);
+		}
+
+	}
+	free(ht->array);
+	free(ht);
+}
