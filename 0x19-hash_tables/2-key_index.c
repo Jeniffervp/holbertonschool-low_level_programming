b464839e38f3ea0db916f2e4bf29e75022dc5eb7
jeniffervp <737@holbertonschool.com>
2019-05-31 19:09:35 +0000

    [Feature]index of a key

---
+++ 0x19-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - find the index of a key.
+ * @key: is the key
+ * @size: size of the array
+ * Return: index
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+{
+	int a;
+	unsigned long int index = 0;
+
+	index = hash_djb2(key) % size;
+	return (index);
+}
