54b8374bd0f4091d8e1ecf408b7d34ffd6bdedab
Jeniffervp <737@holbertonschool.com>
2019-07-24 19:19:10 +0000

    [fix]add null case for func

--- 0x1D-binary_trees/101-binary_tree_levelorder.c
+++ 0x1D-binary_trees/101-binary_tree_levelorder.c
@@ -9,8 +9,8 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t h_left;
-	size_t h_right;
+	size_t h_left = 0;
+	size_t h_right = 0;
 
 	if (tree == NULL)
 		return (0);
@@ -34,7 +34,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 void print_level(const binary_tree_t *tree, int lev, void(*func)(int))
 {
-	if (tree == NULL)
+	if (!tree || !func)
 		return;
 	if (lev == 1)
 		func(tree->n);
