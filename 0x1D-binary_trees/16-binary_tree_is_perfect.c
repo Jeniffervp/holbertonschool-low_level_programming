cc4a1e9e59a7836e77492c2fd4c46dfc0c07d40f
Jeniffervp <737@holbertonschool.com>
2019-07-24 19:00:18 +0000

    [Fix]null case

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -12,6 +12,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t h_left;
 	size_t h_right;
 
+	if (!tree)
+		return (0);
+
 	h_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	h_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	return (h_left > h_right ? h_left : h_right);
