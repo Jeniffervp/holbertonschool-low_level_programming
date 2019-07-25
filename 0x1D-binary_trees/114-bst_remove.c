fd37be0f11336667f75f10063bb8392456ca5422
Jeniffervp <737@holbertonschool.com>
2019-07-25 00:04:44 +0000

    [Feature] half of the remove a node

---
+++ 0x1D-binary_trees/114-bst_remove.c
@@ -0,0 +1,60 @@
+#include "binary_trees.h"
+#include <limits.h>
+/**
+ * minor_val - Search for the minus value in a tree
+ *
+ * @root: pointer to the root node of the tree where you will remove a node
+ * Return: minor value found
+ */
+bst_t *minor_val(bst_t *root)
+{
+	if (root == NULL)
+		return (NULL);
+	else if (root->left != NULL)
+		return (minor_val(root->left));
+	return (root);
+}
+
+/**
+ * bst_remove - function that searches for a value in a Binary Search Tree
+ *
+ * @root: pointer to the root node of the tree where you will remove a node
+ * @value: value to remove in the tree
+ * Return: pointer to the new root node of the tree after removing
+ * the desired value
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+	bst_t *temp;
+
+	if (root == NULL)
+		return (NULL);
+	if (value < root->n)
+		root->left = bst_remove(root->left, value);
+	else if (value > root->n)
+		root->right = bst_remove(root->right, value);
+	else
+	{
+		if (root->left == NULL && root->right == NULL)
+		{
+			free(root);
+			return (NULL);
+		}
+		else if (root->left == NULL || root->right == NULL)
+		{
+			if (root->left == NULL)
+				temp = root->right;
+			else
+				temp = root->left;
+			free(root);
+			return (temp);
+		}
+		else
+		{
+			temp = minor_val(root->right);
+			root->n = temp->n;
+			root->right = bst_remove(root->right, temp->n);
+		}
+	}
+	return (root);
+}
